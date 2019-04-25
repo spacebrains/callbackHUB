52b68e7c3b6f32f9a4d00139e43fc2113dea98c4
Sergei <f.glomgold@yandex.ru>
2019-04-25 22:15:32 +0300

    version 0.2.1

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,6 +1,6 @@
 import {createStore} from 'redux'
 import stateData from './initialState'
-
+import {posts} from './reducer'
 const storeFactory = (initialState=stateData)=>
     createStore(posts,initialState);
 
